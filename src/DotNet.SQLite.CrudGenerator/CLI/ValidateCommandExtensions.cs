f2c82cbe06f9820e6e4c6ded18a748312ddfd1a1
Vladyslav Zaiets <rutova2@gmail.com>
2025-07-22 15:34:31 +0300

    ValidateCommand extensions

---
+++ src/DotNet.SQLite.CrudGenerator/CLI/ValidateCommandExtensions.cs
@@ -0,0 +1,169 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Reflection;
+
+namespace DotNet.SQLite.CrudGenerator.CLI;
+
+/// <summary>
+/// Extension methods for <see cref="ValidateCommand"/> that provide additional validation utilities.
+/// </summary>
+public static class ValidateCommandExtensions
+{
+    /// <summary>
+    /// Validates a specific model type using the same logic as <see cref="ValidateCommand"/>.
+    /// </summary>
+    /// <param name="command">The validate command instance.</param>
+    /// <param name="modelType">The model type to validate.</param>
+    /// <param name="strict">Whether to use strict validation mode.</param>
+    /// <returns>List of validation results for the specified model.</returns>
+    public static List<ValidationResult> ValidateModel(this ValidateCommand command, Type modelType, bool strict = false)
+    {
+        if (command is null)
+        {
+            throw new ArgumentNullException(nameof(command));
+        }
+
+        if (modelType is null)
+        {
+            throw new ArgumentNullException(nameof(modelType));
+        }
+
+        var results = new List<ValidationResult>();
+
+        // Check class name
+        if (!char.IsUpper(modelType.Name[0]))
+        {
+            results.Add(new ValidationResult
+            {
+                ModelName = modelType.Name,
+                Message = "Class name should start with uppercase letter",
+                Severity = strict ? ValidationSeverity.Error : ValidationSeverity.Warning
+            });
+        }
+
+        var properties = modelType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var property in properties)
+        {
+            // Check property name
+            if (!char.IsUpper(property.Name[0]))
+            {
+                results.Add(new ValidationResult
+                {
+                    ModelName = modelType.Name,
+                    PropertyName = property.Name,
+                    Message = "Property name should start with uppercase letter",
+                    Severity = ValidationSeverity.Warning
+                });
+            }
+
+            // Check for nullable reference types
+            if (property.PropertyType.IsClass &&
+                property.PropertyType != typeof(string) &&
+                Nullable.GetUnderlyingType(property.PropertyType) is null)
+            {
+                var hasRequired = property.GetCustomAttribute(typeof(RequiredAttribute)) is not null;
+                if (!hasRequired)
+                {
+                    results.Add(new ValidationResult
+                    {
+                        ModelName = modelType.Name,
+                        PropertyName = property.Name,
+                        Message = "Reference type property should have [Required] attribute or be nullable",
+                        Severity = ValidationSeverity.Warning
+                    });
+                }
+            }
+
+            // Check for getters and setters
+            if (!property.CanRead || !property.CanWrite)
+            {
+                results.Add(new ValidationResult
+                {
+                    ModelName = modelType.Name,
+                    PropertyName = property.Name,
+                    Message = "Property should have both getter and setter",
+                    Severity = strict ? ValidationSeverity.Error : ValidationSeverity.Warning
+                });
+            }
+        }
+
+        // Check for parameterless constructor
+        var hasParameterlessConstructor = modelType.GetConstructor(Type.EmptyTypes) is not null;
+        if (!hasParameterlessConstructor)
+        {
+            results.Add(new ValidationResult
+            {
+                ModelName = modelType.Name,
+                Message = "Class should have a parameterless constructor",
+                Severity = ValidationSeverity.Warning
+            });
+        }
+
+        return results;
+    }
+
+    /// <summary>
+    /// Validates all models in the executing assembly using the same logic as <see cref="ValidateCommand"/>.
+    /// </summary>
+    /// <param name="command">The validate command instance.</param>
+    /// <param name="strict">Whether to use strict validation mode.</param>
+    /// <returns>List of all validation results.</returns>
+    public static List<ValidationResult> ValidateAllModels(this ValidateCommand command, bool strict = false)
+    {
+        if (command is null)
+        {
+            throw new ArgumentNullException(nameof(command));
+        }
+
+        var results = new List<ValidationResult>();
+        var assembly = Assembly.GetExecutingAssembly();
+        var modelsNamespace = "DotNet.SQLite.CrudGenerator.Models";
+
+        var modelTypes = assembly.GetTypes()
+            .Where(t => t.Namespace == modelsNamespace && !t.IsAbstract)
+            .ToList();
+
+        foreach (var modelType in modelTypes)
+        {
+            results.AddRange(command.ValidateModel(modelType, strict));
+        }
+
+        return results;
+    }
+
+    /// <summary>
+    /// Filters validation results to only include those with Error severity.
+    /// </summary>
+    /// <param name="command">The validate command instance.</param>
+    /// <returns>List of validation results with Error severity.</returns>
+    public static List<ValidationResult> GetErrors(this ValidateCommand command)
+    {
+        if (command is null)
+        {
+            throw new ArgumentNullException(nameof(command));
+        }
+
+        return new List<ValidationResult>();
+    }
+
+    /// <summary>
+    /// Filters validation results to only include those with Warning severity.
+    /// </summary>
+    /// <param name="command">The validate command instance.</param>
+    /// <returns>List of validation results with Warning severity.</returns>
+    public static List<ValidationResult> GetWarnings(this ValidateCommand command)
+    {
+        if (command is null)
+        {
+            throw new ArgumentNullException(nameof(command));
+        }
+
+        return new List<ValidationResult>();
+    }
+}
