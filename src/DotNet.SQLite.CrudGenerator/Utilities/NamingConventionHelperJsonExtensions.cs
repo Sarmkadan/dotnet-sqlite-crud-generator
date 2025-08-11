42a06872718437c6e335b5a3d64fcfd073152554
Vladyslav Zaiets <rutova2@gmail.com>
2025-08-11 19:35:10 +0300

    feat: json serialization for NamingConventionHelperJson

---
+++ src/DotNet.SQLite.CrudGenerator/Utilities/NamingConventionHelperJsonExtensions.cs
@@ -0,0 +1,135 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// ====================================================================
+
+using System.Text.Json;
+
+namespace DotNet.SQLite.CrudGenerator.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for naming convention data structures.
+/// </summary>
+public static class NamingConventionHelperJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        WriteIndented = false,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="NamingConventionInfo"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the <see cref="NamingConventionInfo"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this NamingConventionInfo value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="NamingConventionInfo"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="NamingConventionInfo"/> instance populated from the JSON data.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static NamingConventionInfo? FromJsonToNamingConventionInfo(this string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<NamingConventionInfo>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="NamingConventionInfo"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful; otherwise, null.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(this string json, out NamingConventionInfo? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<NamingConventionInfo>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Serializes a <see cref="PropertyConventionInfo"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the <see cref="PropertyConventionInfo"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this PropertyConventionInfo value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="PropertyConventionInfo"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="PropertyConventionInfo"/> instance populated from the JSON data.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static PropertyConventionInfo? FromJsonToPropertyConventionInfo(this string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<PropertyConventionInfo>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="PropertyConventionInfo"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful; otherwise, null.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(this string json, out PropertyConventionInfo? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<PropertyConventionInfo>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
