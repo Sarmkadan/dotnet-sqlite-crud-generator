b9e4a481236ea7407eff8d73802c0edd333f31ea
Vladyslav Zaiets <rutova2@gmail.com>
2025-09-19 09:36:09 +0300

    refactor(dotnet-sqlite-crud-generator.tests): dedupe some logic in Concr

--- tests/dotnet-sqlite-crud-generator.Tests/ConcreteRepositories.cs
+++ tests/dotnet-sqlite-crud-generator.Tests/ConcreteRepositories.cs
@@ -9,15 +9,29 @@ using DotNet.SQLite.CrudGenerator.Models;
 
 namespace DotNet.SQLite.CrudGenerator.Tests;
 
+/// <summary>
+/// Concrete implementation of the <see cref="Repository{T, TKey}"/> for the <see cref="Product"/> entity.
+/// </summary>
 public sealed class ConcreteProductRepository : Repository<Product, int>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConcreteProductRepository"/> class.
+    /// </summary>
+    /// <param name="database">The database connection to use.</param>
     public ConcreteProductRepository(DatabaseConnection database) : base(database)
     {
     }
 }
 
+/// <summary>
+/// Concrete implementation of the <see cref="Repository{T, TKey}"/> for the <see cref="User"/> entity.
+/// </summary>
 public sealed class ConcreteUserRepository : Repository<User, int>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConcreteUserRepository"/> class.
+    /// </summary>
+    /// <param name="database">The database connection to use.</param>
     public ConcreteUserRepository(DatabaseConnection database) : base(database)
     {
     }
