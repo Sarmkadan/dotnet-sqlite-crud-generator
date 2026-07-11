9fe5a96508b5e2e3a0dd5d2140f51b5de051276e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-11 14:04:26 +0000

    refactor(attributes): tighten guard clauses and docs in GenerateGrpcAttributeExtensions

--- src/DotNet.SQLite.CrudGenerator/Attributes/GenerateGrpcAttributeExtensions.cs
+++ src/DotNet.SQLite.CrudGenerator/Attributes/GenerateGrpcAttributeExtensions.cs
@@ -1,15 +1,18 @@
 using System;
-using System.Linq;
 
 namespace DotNet.SQLite.CrudGenerator.Attributes
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="GenerateGrpcAttribute"/> to simplify gRPC service generation configuration.
+    /// </summary>
     public static class GenerateGrpcAttributeExtensions
     {
         /// <summary>
-        /// Determines if the gRPC service should include CRUD operations based on the GenerateCrud flag.
+        /// Determines if the gRPC service should include CRUD operations based on the <see cref="GenerateGrpcAttribute.GenerateCrud"/> flag.
         /// </summary>
-        /// <param name="attribute">The GenerateGrpcAttribute instance</param>
-        /// <returns>True if CRUD operations should be generated; otherwise false</returns>
+        /// <param name="attribute">The <see cref="GenerateGrpcAttribute"/> instance. Cannot be null.</param>
+        /// <returns>True if CRUD operations should be generated; otherwise false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="attribute"/> is null.</exception>
         public static bool ShouldGenerateCrudOperations(this GenerateGrpcAttribute attribute)
         {
             ArgumentNullException.ThrowIfNull(attribute);
@@ -17,10 +20,11 @@ namespace DotNet.SQLite.CrudGenerator.Attributes
         }
 
         /// <summary>
-        /// Determines if the gRPC service should include asynchronous methods based on the GenerateAsync flag.
+        /// Determines if the gRPC service should include asynchronous methods based on the <see cref="GenerateGrpcAttribute.GenerateAsync"/> flag.
         /// </summary>
-        /// <param name="attribute">The GenerateGrpcAttribute instance</param>
-        /// <returns>True if asynchronous methods should be generated; otherwise false</returns>
+        /// <param name="attribute">The <see cref="GenerateGrpcAttribute"/> instance. Cannot be null.</param>
+        /// <returns>True if asynchronous methods should be generated; otherwise false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="attribute"/> is null.</exception>
         public static bool ShouldGenerateAsyncMethods(this GenerateGrpcAttribute attribute)
         {
             ArgumentNullException.ThrowIfNull(attribute);
@@ -30,9 +34,10 @@ namespace DotNet.SQLite.CrudGenerator.Attributes
         /// <summary>
         /// Gets the effective service name, falling back to the entity type name if not specified.
         /// </summary>
-        /// <param name="attribute">The GenerateGrpcAttribute instance</param>
-        /// <param name="entityTypeName">The name of the entity type</param>
-        /// <returns>The service name to use for the gRPC service</returns>
+        /// <param name="attribute">The <see cref="GenerateGrpcAttribute"/> instance. Cannot be null.</param>
+        /// <param name="entityTypeName">The name of the entity type. Cannot be null.</param>
+        /// <returns>The service name to use for the gRPC service.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="attribute"/> or <paramref name="entityTypeName"/> is null.</exception>
         public static string GetEffectiveServiceName(this GenerateGrpcAttribute attribute, string entityTypeName)
         {
             ArgumentNullException.ThrowIfNull(attribute);
@@ -42,10 +47,11 @@ namespace DotNet.SQLite.CrudGenerator.Attributes
         }
 
         /// <summary>
-        /// Determines if the gRPC service should include streaming operations based on the GenerateStreaming flag.
+        /// Determines if the gRPC service should include streaming operations based on the <see cref="GenerateGrpcAttribute.GenerateStreaming"/> flag.
         /// </summary>
-        /// <param name="attribute">The GenerateGrpcAttribute instance</param>
-        /// <returns>True if streaming operations should be generated; otherwise false</returns>
+        /// <param name="attribute">The <see cref="GenerateGrpcAttribute"/> instance. Cannot be null.</param>
+        /// <returns>True if streaming operations should be generated; otherwise false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="attribute"/> is null.</exception>
         public static bool ShouldGenerateStreamingOperations(this GenerateGrpcAttribute attribute)
         {
             ArgumentNullException.ThrowIfNull(attribute);
