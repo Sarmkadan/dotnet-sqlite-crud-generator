93ff143cc8446e0a64c5f300af7c89baa3a1c0c6
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Small refactor - PerformanceMonitorJsonExtensions

--- src/DotNet.SQLite.CrudGenerator/Utilities/PerformanceMonitorJsonExtensions.cs
+++ src/DotNet.SQLite.CrudGenerator/Utilities/PerformanceMonitorJsonExtensions.cs
@@ -31,16 +31,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the performance monitor.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this PerformanceMonitor value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this PerformanceMonitor value, bool indented = false) => JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true } : _jsonOptions);
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="PerformanceMonitor"/> instance.
@@ -49,12 +40,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <returns>A <see cref="PerformanceMonitor"/> instance populated from the JSON data.</returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static PerformanceMonitor? FromJson(string json)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
-
-        return JsonSerializer.Deserialize<PerformanceMonitor>(json, _jsonOptions);
-    }
+    public static PerformanceMonitor? FromJson(string json) => JsonSerializer.Deserialize<PerformanceMonitor>(json, _jsonOptions);
 
     /// <summary>
     /// Attempts to deserialize a JSON string to a <see cref="PerformanceMonitor"/> instance.
@@ -62,7 +48,8 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized <see cref="PerformanceMonitor"/> instance if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out PerformanceMonitor? value)
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJsonToPerformanceMonitor(string json, out PerformanceMonitor? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
@@ -85,16 +72,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the operation metrics.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this OperationMetrics value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this OperationMetrics value, bool indented = false) => JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true } : _jsonOptions);
 
     /// <summary>
     /// Deserializes a JSON string to an <see cref="OperationMetrics"/> instance.
@@ -103,12 +81,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <returns>An <see cref="OperationMetrics"/> instance populated from the JSON data.</returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static OperationMetrics? FromJsonToOperationMetrics(string json)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
-
-        return JsonSerializer.Deserialize<OperationMetrics>(json, _jsonOptions);
-    }
+    public static OperationMetrics? FromJsonToOperationMetrics(string json) => JsonSerializer.Deserialize<OperationMetrics>(json, _jsonOptions);
 
     /// <summary>
     /// Attempts to deserialize a JSON string to an <see cref="OperationMetrics"/> instance.
@@ -116,7 +89,8 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized <see cref="OperationMetrics"/> instance if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out OperationMetrics? value)
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJsonToOperationMetrics(string json, out OperationMetrics? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
@@ -139,16 +113,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the performance report.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this PerformanceReport value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this PerformanceReport value, bool indented = false) => JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true } : _jsonOptions);
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="PerformanceReport"/> instance.
@@ -157,12 +122,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <returns>A <see cref="PerformanceReport"/> instance populated from the JSON data.</returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static PerformanceReport? FromJsonToPerformanceReport(string json)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
-
-        return JsonSerializer.Deserialize<PerformanceReport>(json, _jsonOptions);
-    }
+    public static PerformanceReport? FromJsonToPerformanceReport(string json) => JsonSerializer.Deserialize<PerformanceReport>(json, _jsonOptions);
 
     /// <summary>
     /// Attempts to deserialize a JSON string to a <see cref="PerformanceReport"/> instance.
@@ -170,7 +130,8 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized <see cref="PerformanceReport"/> instance if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out PerformanceReport? value)
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJsonToPerformanceReport(string json, out PerformanceReport? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
@@ -193,16 +154,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the memory info.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this MemoryInfo value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this MemoryInfo value, bool indented = false) => JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true } : _jsonOptions);
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="MemoryInfo"/> instance.
@@ -211,12 +163,7 @@ public static class PerformanceMonitorJsonExtensions
     /// <returns>A <see cref="MemoryInfo"/> instance populated from the JSON data.</returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static MemoryInfo? FromJsonToMemoryInfo(string json)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
-
-        return JsonSerializer.Deserialize<MemoryInfo>(json, _jsonOptions);
-    }
+    public static MemoryInfo? FromJsonToMemoryInfo(string json) => JsonSerializer.Deserialize<MemoryInfo>(json, _jsonOptions);
 
     /// <summary>
     /// Attempts to deserialize a JSON string to a <see cref="MemoryInfo"/> instance.
@@ -224,7 +171,8 @@ public static class PerformanceMonitorJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized <see cref="MemoryInfo"/> instance if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out MemoryInfo? value)
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJsonToMemoryInfo(string json, out MemoryInfo? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
 
